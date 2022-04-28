15de77a8c88cc2eb2b26ddc67c185cab36e8d0c9
solwpicard723 <solwpicard723@gmail.com>
2022-04-28 06:14:33 -0600

    Latest smart contract

--- src/config/constants/nfts/index.ts
+++
@@ -1,9 +0,0 @@
-import gladiatorsProfileCollectibles from './gladiatorsProfileCollectibles'
-import { CollectionKey, Nfts } from './types'
-
-const nfts: Nfts = {
-  [CollectionKey.GLADIATORS]: gladiatorsProfileCollectibles,
-}
-
-export default nfts
-export { default as collections } from './collections'
